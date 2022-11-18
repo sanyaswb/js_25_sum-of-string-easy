4155995a0ff41793b43b0ea382104b2c9ebe7420
sanyaswb <sanyaswb@gmail.com>
2022-11-18 04:52:15 +0200

    completed 25_sum-of-string-easy

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function sumOfString(str) {
-	// write your code here
+	let result = 0;
+
+	for (let i = 0; i < str.length; i++) {
+		result += Number(str[i]);
+	}
+
+	return result;
 }
 
 module.exports = sumOfString;
